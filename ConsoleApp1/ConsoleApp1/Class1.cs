31fd9b4ec89172e093d1a5b5a5f75268f58c984a
teruki.yonashiro <teruki.yonahiro31@dcom-net.jp>
2021-08-24 18:00:20 +0900

    新規

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public Class1()
+        {
+           
+                lis = new List<string>();
+                lis.Add("AAA");
+                lis.Add("BBB");
+
+                
+            
+        }
+   
+        public List<string> lis { get; set; }
+       
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    // linq練習
+    class Program
+    {
+        　
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            // foreach
+            class1.lis.ForEach(s => hyouzi(s));
+            // where ToLower
+            IEnumerable<string> whereTestList = class1.lis.Where(s => s.Length <= 3)
+                .Select(s => s.ToLower());
+            foreach(var s in whereTestList)
+            {
+                Console.WriteLine(s);
+            }
+            // select 文字数表示
+            var selectTestList = whereTestList.Select(s => s.Length);
+            foreach (var s in selectTestList)
+            {
+                Console.WriteLine(s);
+            }
+
+            // 
+        }
+        // 表示メソッド
+        public static void hyouzi(string s)
+        { 
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+
+    }
+}
