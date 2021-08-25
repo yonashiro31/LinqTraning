2a4d25841556755129740b3691a08088f1988416
teruki.yonashiro <teruki.yonahiro31@dcom-net.jp>
2021-08-25 17:51:15 +0900

    変更

--- ConsoleApp1/ConsoleApp1/Class1.cs
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -1,24 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ConsoleApp1
 {
     class Class1
     {
+
         public Class1()
         {
-           
-                lis = new List<string>();
-                lis.Add("AAA");
-                lis.Add("BBB");
+            lis = new List<string>();
+            lis.Add("AAA");
+            lis.Add("BBB");
 
-                
-            
+            // Enumberable.Repeat 同じ値で埋める
+            repeatList = Enumerable.Repeat("繰り返す", 3)
+                 .ToList();
+            // Enumbarable.Range　連続した値で埋める
+            rangeList = Enumerable.Range(1, 4)
+                .ToArray();
         }
-   
+
         public List<string> lis { get; set; }
-       
+        public List<string> repeatList { get; set; }
+        public int[] rangeList { get; set; }
+
 
     }
 }
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,19 +4,21 @@ using System.Linq;
 
 namespace ConsoleApp1
 {
-    // linq練習
+    /// <summary>
+    /// Linq練習
+    /// </summary>
     class Program
     {
-        　
+
         static void Main(string[] args)
         {
             Class1 class1 = new Class1();
             // foreach
-            class1.lis.ForEach(s => hyouzi(s));
+            class1.lis.ForEach(s => strHyouzi(s));
             // where ToLower
             IEnumerable<string> whereTestList = class1.lis.Where(s => s.Length <= 3)
                 .Select(s => s.ToLower());
-            foreach(var s in whereTestList)
+            foreach (var s in whereTestList)
             {
                 Console.WriteLine(s);
             }
@@ -26,12 +28,52 @@ namespace ConsoleApp1
             {
                 Console.WriteLine(s);
             }
+            Console.WriteLine();
 
-            // 
+            class1.repeatList.ForEach(s => strHyouzi(s));
+
+            foreach (var s in class1.rangeList)
+            {
+                intHyouzi(s);
+            }
+            var numbers = new List<int> { 1, 2, 3, 4, 5 };
+            var ave = numbers.Average();
+            Console.WriteLine(ave);
+            // 最小値最大値抽出
+            var minNum = numbers.Where(s => s >= 3)
+            .Min();
+            var maxNum = numbers.Max();
+            Console.WriteLine(minNum);
+            Console.WriteLine(maxNum);
+            // 条件を満たす要素数を数える
+            var a = numbers.Count(s => s == 3);
+            Console.WriteLine(a);
+            //　条件に一致する要素があるか調べる
+            var check = numbers.Any(s => s == 5);
+            Console.WriteLine(check);
+            // 条件に一致する要素を指定数抽出
+            var nums = numbers.Where(s => s >= 2)
+                .Take(2);
+            Console.WriteLine(nums.Count());
+
+
+
+
         }
+
+
+
+        //
+
         // 表示メソッド
-        public static void hyouzi(string s)
-        { 
+        public static void strHyouzi(string s)
+        {
+            {
+                Console.WriteLine(s);
+            }
+        }
+        public static void intHyouzi(int s)
+        {
             {
                 Console.WriteLine(s);
             }
