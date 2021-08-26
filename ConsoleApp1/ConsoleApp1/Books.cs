93b8a565a45f0a5b883dd56d7fbdaa464439c85d
teruki.yonashiro <teruki.yonahiro31@dcom-net.jp>
2021-08-26 18:02:14 +0900

    変更

---
+++ ConsoleApp1/ConsoleApp1/Books.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Books
+    {
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public int Pages { get; set; }
+        public List<Books> books { get; set; }
+        public  void Book(){
+            books = new List<Books>()
+            {
+                
+                new Books{Title = "C#プログラミングの新常識", Price = 3800 ,Pages = 378},
+                new Books{Title = "ラムダ式とLINQの極意", Price = 2500 ,Pages = 385},
+                new Books{Title = "ワンダフルC#ライフ", Price = 2900 ,Pages = 385},
+                new Books{Title = "一人で学ぶ並列処理（略", Price =4800 ,Pages =464 },
+                new Books{Title = "フレーズ（略", Price = 5300 ,Pages =604 },
+                new Books{Title = "私でも分かったASP（略", Price =3200  ,Pages = 453},
+                new Books{Title = "楽しいC#（略", Price = 2540 ,Pages = 348}
+            };
+
+            // 教本問題②
+            // ワンダフル（略の価格とページ数を表示
+            var wanderDate = books.Where(item => item.Title == "ワンダフルC#ライフ").ToList();
+            Console.WriteLine("ワンダフルの価格とページ数：" + wanderDate[0].Price + " "  +wanderDate[0].Pages );
+            // C＃が含まれている冊数をカウント
+            var csCount = books.Count(item => item.Title.Contains("C#"));
+            Console.WriteLine("C#が含まれている冊数" + csCount);
+            // C#が含まれている本の平均ページ
+            var csBooks = books.Where(item => item.Title.Contains("C#")).Average(item => item.Pages);
+
+            var List1 = new List<int>();
+            foreach (var item in books)
+            {
+                if (item.Title.Contains("C#"))
+                {
+                    List1.Add(item.Pages);                    
+                }               
+            }
+            var num2 = 0;
+            var num = 0;
+            foreach(var i in List1)
+            {
+                 num2++;
+                 num += i;                 
+            }
+            var aaaa = num / num2;
+            // 価格が400以上で最初に見つかった書籍のタイトルを表示
+          
+
+        }
+       
+
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -56,8 +56,41 @@ namespace ConsoleApp1
                 .Take(2);
             Console.WriteLine(nums.Count());
 
+            // 教本練習問題
+            var traningNumbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
 
+            // 1，最大値を求め、表示してください
+            var maxNum2 = traningNumbers.Max();
+            Console.WriteLine("最大値" + maxNum2);
+            // 2,最後から二つの要素を取り出して表示してください
+            var last2Num = traningNumbers.Reverse().Take(2);
+            Console.WriteLine("最後から要素を二つ取り出す");
+            foreach (var item in last2Num)
+            {
+                Console.WriteLine(item);
+            }
 
+            // 3,それぞれの数値を文字列に変換し結果を表示してください
+            var stringNum = traningNumbers.Select(item => item.ToString()).ToList();
+            Console.WriteLine("数値を文字列にして表示");
+            stringNum.ForEach(item =>
+            Console.WriteLine(item));
+        
+            // 4,数の小さい順に並べ、先頭から3つを取り出し結果を表示してください
+            var sortList = traningNumbers.OrderBy(item => item);
+            var min3Num = sortList.Take(3);
+            foreach (var item in min3Num)
+            {
+                Console.WriteLine("降順かつ先頭から3つ取り出した数値" +  item);
+            }
+                // 5,重複を排除した後、10より大きい値がいくつあるのかカウントし、結果を表示してください。
+            var testNumbers = traningNumbers.Distinct();
+            var over10Num = testNumbers.Count(item => item > 10);
+            Console.WriteLine("重複排除10以上の数値の数" + over10Num);
+            Books b = new Books();
+            b.Book();
+            
+           
 
         }
 
